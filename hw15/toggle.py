957e4aa302c06944d8f283970a44ba17f6cdb730
parksh8009 <185870146+parksh8009@users.noreply.github.com>
2025-05-16 20:23:02 +0900

    hw15

---
+++ hw15/toggle.py
@@ -0,0 +1,34 @@
+def toggle_text(text: str) -> str:
+    result = []
+    for ch in text:
+        if 'a' <= ch <= 'z':
+            result.append(chr(ord(ch) - 32))  
+        elif 'A' <= ch <= 'Z':
+            result.append(chr(ord(ch) + 32))  
+        else:
+            result.append(ch)  
+    return ''.join(result)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+def toggle_text(text: str) -> str:
+    result = []
+    for ch in text:
+        if 'a' <= ch <= 'z':
+            result.append(chr(ord(ch) - 32))  # 소문자 → 대문자
+        elif 'A' <= ch <= 'Z':
+            result.append(chr(ord(ch) + 32))  # 대문자 → 소문자
+        else:
+            result.append(ch)  # 알파벳 아닌 문자
+    return ''.join(result)
+
+def main():
+    sample=input("영문자 입력 : ")
+    print("변환 결과:", toggle_text(sample))  # hELLO pYTHON!
+
+if __name__ == "__main__":
+    main()
